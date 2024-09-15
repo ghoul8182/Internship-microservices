3338979487b9455d30aba02eb38563a236d3431d
Mohammad Ghoul <aalghool090@gmail.com>
2024-09-15 08:35:46 +0300

    fix the naming

--- src/main/java/com/example/sms_app/appconfig/Appconfig.java
+++ src/main/java/com/example/sms_app/appconfig/Appconfig.java
@@ -1,4 +1,4 @@
-package com.example.sms_app.appcon;
+package com.example.sms_app.appconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
