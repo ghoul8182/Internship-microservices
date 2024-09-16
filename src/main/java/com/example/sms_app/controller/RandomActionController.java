a9322183e449a9121d9deac3848cfd7fc9a3d7f8
Mohammad Ghoul <aalghool090@gmail.com>
2024-09-16 22:54:37 +0300

    Adding the random controller

--- src/main/java/com/example/sms_app/controller/RandomActionController.java
+++ src/main/java/com/example/sms_app/controller/RandomActionController.java
@@ -1,4 +1,39 @@
 package com.example.sms_app.controller;
 
+import com.example.sms_app.Service.Provider1Service;
+import com.example.sms_app.Service.Provider2Service;
+import com.example.sms_app.exception.CustomException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/api/random")
 public class RandomActionController {
+
+    @Autowired
+    private Provider1Service provider1Service;
+
+    @Autowired
+    private Provider2Service provider2Service;
+
+    private static final Random RANDOM = new Random();
+
+    @PostMapping
+    public String performRandomAction(@RequestBody String message) {
+        int action = RANDOM.nextInt(3); // Randomly choose 0, 1, or 2
+        switch (action) {
+            case 0:
+                provider1Service.sendSMS(message);
+                return "Message sent via Provider 1";
+            case 1:
+                provider2Service.sendSMS(message);
+                return "Message sent via Provider 2";
+            case 2:
+                throw new CustomException("Randomly thrown custom exception");
+            default:
+                throw new CustomException("Unexpected error");
+        }
+    }
 }
