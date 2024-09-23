798c92c5f9d5a3b9f20b3ba9f3dd211a7d59862f
Mohammad Ghoul <aalghool090@gmail.com>
2024-09-23 22:56:25 +0300

    fix the try catch

--- src/main/java/com/example/sms_app/Service/QueueService.java
+++ src/main/java/com/example/sms_app/Service/QueueService.java
@@ -14,13 +14,22 @@ public class QueueService {
 
     public void sendToQueue1(String message) {
         String routingKeyQueue1 = "queue1";
-        rabbitTemplate.convertAndSend(EXCHANGE, routingKeyQueue1, message);
-        System.out.println("Message sent to Queue 1: " + message);
+        try {
+            rabbitTemplate.convertAndSend(EXCHANGE, routingKeyQueue1, message);
+            System.out.println("Message sent to Queue 1: " + message);
+        } catch (Exception e) {
+            System.err.println("Error sending message to Queue 1: " + e.getMessage());
+        }
     }
 
     public void sendToQueue2(String message) {
         String routingKeyQueue2 = "queue2";
-        rabbitTemplate.convertAndSend(EXCHANGE, routingKeyQueue2, message);
-        System.out.println("Message sent to Queue 2: " + message);
+        try {
+            rabbitTemplate.convertAndSend(EXCHANGE, routingKeyQueue2, message);
+            System.out.println("Message sent to Queue 2: " + message);
+        } catch (Exception e) {
+            System.err.println("Error sending message to Queue 2: " + e.getMessage());
+        }
     }
 }
+
