bba88f1116e9a9d123267106703f1be1a1eea47d
Mohammad Ghoul <aalghool090@gmail.com>
2024-09-23 11:14:59 +0300

    edit the schedluedService to consume messages from queue 2 every 10 seconds

--- src/main/java/com/example/sms_app/Service/ScheduledService.java
+++ src/main/java/com/example/sms_app/Service/ScheduledService.java
@@ -1,6 +1,7 @@
 package com.example.sms_app.Service;
 
 import com.example.sms_app.Util.CodeGenerator;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,21 @@ public class ScheduledService {
 
     @Autowired
     private SmsService smsService;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
 
     @Scheduled(fixedRate = 3600000) // 1 hour
     public void sendScheduledSms() {
         String randomCode = CodeGenerator.generateRandomCode();
         String message = "Your current code is " + randomCode;
         smsService.sendAndSaveSms(message);
+    }
+    @Scheduled(fixedRate = 10000) // 10 seconds
+    public void consumeQueue2() {
+        String message = (String) rabbitTemplate.receiveAndConvert("queue2");
+        if (message != null) {
+            System.out.println("Consumed from Queue 2: " + message);
+        }
     }
 }
