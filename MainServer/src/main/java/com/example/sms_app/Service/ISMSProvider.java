c9c6d7ea593f31f015da7ff362bfde87ca14bb60
mhamadelitawi <melitawi@eddress.co>
2024-10-08 19:48:38 +0300

    restructure the code

---
+++ MainServer/src/main/java/com/example/sms_app/Service/ISMSProvider.java
@@ -0,0 +1,6 @@
+package com.example.sms_app.Service;
+
+public interface ISMSProvider {
+    void sendSMS(String message);
+
+}
---
+++ MainServer/src/main/java/com/example/sms_app/repository/MessageRepository.java
@@ -0,0 +1,15 @@
+package com.example.sms_app.repository;
+
+import com.example.sms_app.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+
+
+
+    
+
+}
